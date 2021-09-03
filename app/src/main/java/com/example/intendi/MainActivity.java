06ed6faa544a4142e62ef83676540926f69f2609
Sahid Rosas <serm-2000@hotmail.com>
2021-09-03 01:27:51 -0500

    Pagina Login, función semi-completa

--- app/src/main/java/com/example/intendi/MainActivity.java
+++ app/src/main/java/com/example/intendi/MainActivity.java
@@ -1,8 +1,25 @@
 package com.example.intendi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.databinding.BindingAdapter;
+import androidx.databinding.DataBindingUtil;
 
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.security.acl.Group;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +27,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        TableLayout usersGrid = (TableLayout)findViewById(R.id.usersGrid);
+        int users = 0;
+
+        LayoutInflater inflater = (LayoutInflater) getLayoutInflater();
+
+        //BindingAdapter binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        for (int i=0;i<3;i++){
+            TableRow line = (TableRow)usersGrid.getChildAt(i);
+            for(int j=0;j<2;j++)
+            {
+                users+=1;
+                TableRow.LayoutParams vp = new TableRow.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                vp.weight = 1;
+                View  layout = inflater.inflate(R.layout.user_login_space, null);
+
+                TextView textView = (TextView) layout.findViewById(R.id.username);
+                textView.setText("user " + users);
+                ImageView profilePic = (ImageView) layout.findViewById(R.id.userAvatar);
+                profilePic.setImageDrawable(ContextCompat.getDrawable(this, R.mipmap.iguanee));
+                //profilePic.setImageDrawable(Drawable.createFromPath("@mipmap/delphi"));
+
+                layout.setLayoutParams(vp);
+                line.addView(layout,j);
+            }
+        }
+
     }
 }
