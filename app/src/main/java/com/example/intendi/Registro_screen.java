6fb72dcd3d7f71e9fe93979f403e27fb3964daec
alexlohe25 <A01733984@itesm.mx>
2021-09-03 10:12:07 -0500

    Pantalla de registro, paleta de colores en colors.xml

---
+++ app/src/main/java/com/example/intendi/Registro_screen.java
@@ -0,0 +1,13 @@
+package com.example.intendi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Registro_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_screen);
+    }
+}
