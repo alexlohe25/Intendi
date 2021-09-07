379ce966018ef20ac04b9cff01b225946c54f5b7
7Pablo <71157307+7Pablo@users.noreply.github.com>
2021-09-07 15:21:41 -0500

    Pantalla de pregame (Template)

---
+++ app/src/main/java/com/example/intendi/PregameTemplate.java
@@ -0,0 +1,14 @@
+package com.example.intendi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PregameTemplate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pregame_template);
+    }
+}
