0e5847c9941ef1562fe1194872143b0ec4b4682e
DessTEC <A01733753@itesm.mx>
2021-09-07 15:43:34 -0500

    Cambio de size en menu juegos, color del activity y pasar ajustes a un fragmento

--- app/src/main/java/com/example/intendi/AjustesFragment.java
+++ app/src/main/java/com/example/intendi/AjustesFragment.java
@@ -1,5 +1,6 @@
 package com.example.intendi;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,20 +8,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AjustesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AjustesFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
+    Button logOutButton;
+
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -28,15 +25,6 @@ public class AjustesFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AjustesFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static AjustesFragment newInstance(String param1, String param2) {
         AjustesFragment fragment = new AjustesFragment();
         Bundle args = new Bundle();
@@ -53,12 +41,28 @@ public class AjustesFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ajustes, container, false);
+
+         View view = inflater.inflate(R.layout.fragment_ajustes, container, false);
+
+         logOutButton= view.findViewById(R.id.button);
+         logOutButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    logOut();
+                }
+            });
+
+         return view;
+    }
+
+    public void logOut(){
+        Intent miIntent = new Intent( getActivity(), MainActivity.class);
+        startActivity(miIntent);
     }
 }
