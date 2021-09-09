aca88e9356b9de1dd68e469714394c1c3698ce3b
DessTEC <A01733753@itesm.mx>
2021-09-09 13:46:02 -0500

    Agregar instrucciones de juegos y funcionalidad de botón atrás

--- app/src/main/java/com/example/intendi/PregameTemplate.java
+++ app/src/main/java/com/example/intendi/PregameTemplate.java
@@ -2,13 +2,51 @@ package com.example.intendi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class PregameTemplate extends AppCompatActivity {
+
+    ImageView imageBubble;
+    ImageView imageLogo;
+    TextView textBubble;
+    FloatingActionButton go_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pregame_template);
+
+        imageBubble = findViewById(R.id.imageBubble);
+        imageLogo = findViewById(R.id.imageLogo);
+        textBubble = findViewById(R.id.textBubble);
+
+        Intent myIntent = getIntent();
+        String imageBubbleDir = myIntent.getStringExtra("imageBubble");
+        String imageLogoDir = myIntent.getStringExtra("imageLogo");
+        String textBubbleDir = myIntent.getStringExtra("textBubble");
+
+        int idImageBubble = this.getResources().getIdentifier(imageBubbleDir, "drawable", this.getPackageName());
+        int idLogo = this.getResources().getIdentifier(imageLogoDir, "drawable", this.getPackageName());
+
+        imageBubble.setImageResource(idImageBubble);
+        imageLogo.setImageResource(idLogo);
+        textBubble.setText(textBubbleDir);
+
+        go_back = findViewById(R.id.returnButton);
+
+        go_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
