e0bbab757843d44826da028237408e2a7855020d
alexlohe25 <A01733984@itesm.mx>
2021-09-29 14:29:51 -0500

    Version 1.2 Memorama

--- app/src/main/java/com/example/intendi/MemoryCard.java
+++ app/src/main/java/com/example/intendi/MemoryCard.java
@@ -2,10 +2,16 @@ package com.example.intendi;
 
 public class MemoryCard {
     int identifier;
+    int contentIndex;
     boolean isFaceUp = false;
     boolean isMatched = false;
-
+    boolean isSound = false;
     public MemoryCard(Integer id) {
         identifier = id;
+    }
+    public MemoryCard(Integer id, Integer listIndex, boolean sound) {
+        identifier = id;
+        contentIndex = listIndex;
+        isSound = sound;
     }
 }
