f0daaf786bb27ec6423dcaf8896d50987b205cbc
7Pablo <71157307+7Pablo@users.noreply.github.com>
2021-09-30 17:13:42 -0500

    Crear actividad de piano y generar su xml. Se añadio los sonidos al igual que algunas fotos al res.

---
+++ app/src/main/java/com/example/intendi/PianoGame.java
@@ -0,0 +1,14 @@
+package com.example.intendi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PianoGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_piano_game);
+    }
+}
