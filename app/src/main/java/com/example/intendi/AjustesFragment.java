ce34782c10a5691623f3009610354326d04f72be
Sahid Rosas <serm-2000@hotmail.com>
2021-10-07 12:37:17 -0500

    Añade menu de cambio de avatar

--- app/src/main/java/com/example/intendi/AjustesFragment.java
+++ app/src/main/java/com/example/intendi/AjustesFragment.java
@@ -9,10 +9,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class AjustesFragment extends Fragment {
 
     Button logOutButton;
+    ImageView changeAvatarButton;
+    View changeAvatarMenu;
+    ImageView backgroundMenu;
 
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -45,18 +49,33 @@ public class AjustesFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
          View view = inflater.inflate(R.layout.fragment_ajustes, container, false);
 
          logOutButton= view.findViewById(R.id.playButton);
+         changeAvatarButton = view.findViewById(R.id.editAvatar);
+         changeAvatarMenu = view.findViewById(R.id.avatarMenu);
+         backgroundMenu = view.findViewById(R.id.faded_background);
+
          logOutButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     logOut();
                 }
             });
+        changeAvatarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeAvatarMenu.setVisibility(View.VISIBLE);
+            }
+        });
+        backgroundMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeAvatarMenu.setVisibility(View.INVISIBLE);
+            }
+        });
 
          return view;
     }
