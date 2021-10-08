f9799d8bd1b7ed24833d320e5d6b3dffd235d469
DessTEC <A01733753@itesm.mx>
2021-10-08 18:27:51 -0500

    Reformateo de UI de resultados

--- app/src/main/java/com/example/intendi/ResultadosFragment.java
+++ app/src/main/java/com/example/intendi/ResultadosFragment.java
@@ -89,7 +89,7 @@ public class ResultadosFragment extends Fragment {
         result2 = (TextView) view.findViewById(R.id.secondResult);
        // miChart = (LineChart) view.findViewById(R.id.idChart);
         tf = ResourcesCompat.getFont(getActivity(), R.font.nunito_semibold);
-        generaChart("Whack-A-Ball", view);
+        //generaChart("Whack-A-Ball", view);
         currentGame = "Whack-A-Ball";
         cardWhack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -165,90 +165,5 @@ public class ResultadosFragment extends Fragment {
                 tendencia = "¡Buen intento "+ currentUser.getUsername() + " :)!";
             resultDetail.setText(tendencia);
         }
-        /*ArrayList<String> misValoresX = obtenValoresEnX();
-        ArrayList<Entry> misValoresY = obtenValoresEnY();
-        int YArraySize = misValoresY.size();
-
-        LineDataSet set1 = new LineDataSet(misValoresY, "Score del juego");
-        if( YArraySize == 0){
-            resultDetail.setText("Aún no has jugado a "+ game);
-        }else{
-            if(misValoresY.get(YArraySize - 1).getY() >= misValoresY.get(YArraySize - 2).getY()){
-                resultDetail.setText("¡Juegas increíble al Intendi!");
-            }else{
-                resultDetail.setText("¡No te desanimes "+ currentUser.getUsername() +"! La próxima vez será mejor ");
-            }
-        }
-        set1.setDrawCircles(true); //para que los puntos esten llenos y bonitos
-        set1.setDrawCircleHole(false);
-        set1.setLineWidth(5f);
-        set1.setCircleRadius(12f);
-        set1.setValueTextSize(12f);
-        set1.setCircleColor(Color.parseColor("#023047"));
-        set1.setColor(Color.parseColor("#023047"));
-        ArrayList<ILineDataSet> dataSets = new ArrayList<>(); // metes el set de valores en Y en otro array
-        dataSets.add(set1);
-
-        LineData data = new LineData(dataSets); // incias la data del grafico con el arreglo que llenaste un paso antes
-        data.setValueTypeface(tf);
-        data.setValueTextColor(Color.parseColor("#FB8500"));
-        data.setValueFormatter(new ValueFormatter() {
-            public String getFormattedValue(float value) {
-                return "" + ((int) value);
-            }
-        });
-
-        Description chartDesc = new Description();
-        chartDesc.setText("Puntaje en " + game );
-        chartDesc.setTypeface(tf);
-        miChart.setDescription(chartDesc);
-        miChart.getAxisLeft().setDrawGridLines(false);
-        miChart.getXAxis().setDrawGridLines(false);
-        miChart.setPinchZoom(false);//para que no se mueva la grafica (drag ni zoom)
-        miChart.setScaleEnabled(false);
-        miChart.setTouchEnabled(false);
-        miChart.animateY(500);//animar la entrada
-        miChart.getAxisLeft().setTypeface(tf);
-
-        XAxis xAxis = miChart.getXAxis();
-        xAxis.setTypeface(tf);
-        xAxis.setAxisMinimum(0);//seteas el rango en X que debe ser de 0 a listaValoresX().size
-        xAxis.setAxisMaximum(YArraySize);
-        xAxis.setGranularity(1f); //granuralidad de el eje X
-        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
-        xAxis.setTextSize(10f);
-        xAxis.setLabelRotationAngle(45);
-        xAxis.setCenterAxisLabels(true);
-        xAxis.setValueFormatter(new ValueFormatter() { //vaciar los datos del arreglo de valores en X al label del eje X
-            @Override //si, un override
-            public String getAxisLabel(float value, AxisBase axis){
-                if(value >= 0 && value <= misValoresX.size() - 1)
-                    return misValoresX.get((int) value);
-                return "";
-            }
-        });
-        miChart.getAxisRight().setEnabled(false); // que el label del eje Y se vea solo en la izquierda
-        miChart.setData(data); //setea la data que guardaste de valores en Y al grafico
-        miChart.invalidate();
-    }
-    ArrayList<String> obtenValoresEnX(){
-        ArrayList<String> misValoresX = new ArrayList<>();
-        misValoresX.add("01/09/21"); //empieza en i = 0
-        misValoresX.add("02/09/21");
-        misValoresX.add("03/09/21");
-        misValoresX.add("04/09/21");
-        misValoresX.add("04/09/21");
-        misValoresX.add("04/09/21");// i = 5
-        return misValoresX;
-    }
-    ArrayList<Entry> obtenValoresEnY(){
-        ArrayList<Entry> misValoresY = new ArrayList<>();
-        misValoresY.add(new Entry(0.5f,20)); //la posicion en x es i + 0.5 para el diseño
-        misValoresY.add(new Entry(1.5f,15));
-        misValoresY.add(new Entry(2.5f,18));
-        misValoresY.add(new Entry(3.5f,17));
-        misValoresY.add(new Entry(4.5f,21));
-        misValoresY.add(new Entry(5.5f,19));
-        return misValoresY;*/
     }
 }
